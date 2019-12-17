f9711e2c3d790c4a3dfe7387eed400abf9cd34f7
cleDiego <cleversondesign@hotmail.com>
2019-12-17 16:44:30 -0300

    .

--- tjs.js
+++ tjs.js
@@ -2,5 +2,6 @@ $(document).ready(function() {
     $('input[type=checkbox], input[type=radio]').iCheck({
         checkboxClass: 'icheckbox_square-blue',
         radioClass: 'iradio_square-blue',
+        increaseArea: '-20%'
     });
 });
