8c53a17c50fc72fd6349a39035d9043d629ab742
cleDiego <51338201+cleDiego@users.noreply.github.com>
2021-02-18 14:36:19 -0300

    Update tjs.js

--- tjs.js
+++ tjs.js
@@ -34,7 +34,7 @@ $(document).ready(function() {
                     removeHighlighting(this);
                     if($(this).text().toLowerCase().indexOf(value) > -1) {
                         show = true;
-                        addHighlighting($(this), $("#searchProt").val());
+                        addHighlighting($(this), $(this).text());
                     }
                 });
                 
